978e80b6774120616ca538de73137030419e4703
hjvasoya001 <hardik.vasoya@capgemini.com>
2026-06-24 17:45:37 +0000

    Issue fix

---
+++ octofit-tracker/backend/src/server.ts
@@ -0,0 +1,20 @@
+import app from "./app";
+import { connectDb } from "./config/database";
+import { port, apiUrl, codespaceName } from "./config";
+
+const apiHostMessage = codespaceName
+  ? `Codespace-aware API URL set to https://${codespaceName}-8000.app.github.dev`
+  : `Local API URL set to ${apiUrl}`;
+
+connectDb()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Backend listening on port ${port}`);
+      console.log(apiHostMessage);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
