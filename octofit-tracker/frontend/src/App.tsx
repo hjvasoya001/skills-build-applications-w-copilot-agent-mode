2786a499deba14b74080563bd1fbe14f4ff22de9
hjvasoya001 <hardik.vasoya@capgemini.com>
2026-06-24 17:23:55 +0000

    Activity: Initialize frontend and backend package manifests

---
+++ octofit-tracker/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>OctoFit Tracker</h1>
+      <p>Modern multi-tier fitness tracking app frontend.</p>
+    </div>
+  );
+}
+
+export default App;
